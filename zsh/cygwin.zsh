bbbbfda4b31efcad6608dc1f7ac316c8b34f9111
Jason Filsinger <jason.filsinger@gmail.com>
2012-07-18 18:11:14 -0700

    giving 'ls' in cygwin color output

--- zsh/cygwin.zsh
+++ zsh/cygwin.zsh
@@ -3,5 +3,7 @@ if [[ $OSTYPE == 'cygwin' ]]; then
 	# bind HOME and END to beginning and end of line
 	bindkey '\e[1~' beginning-of-line
 	bindkey '\e[4~' end-of-line
+
+    alias ls="ls --color=auto"
 fi
 
