5d814d7eb1b13c1c73ec1061e9b86c16538be857
Jason Filsinger <jason.filsinger@gmail.com>
2013-07-25 14:22:23 -0700

    fixing perforce file paths use in cygwin

--- zsh/cygwin.zsh
+++ zsh/cygwin.zsh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # only do the following if running interactively
 if [[ -n "$PS1" ]]; then
 	if [[ $OSTYPE == 'cygwin' ]]; then
@@ -9,5 +10,12 @@ if [[ -n "$PS1" ]]; then
 
 		# ignore dll files when autocompleting
 		zstyle ':completion:*:complete:-command-:*' ignored-patterns '*.dll'
+
+		if [[ -e "$(cygpath "C:\Program Files\Perforce\p4.exe")" ]]; then
+			function p4() {
+				export PWD=`cygpath -wa .`
+				"$(cygpath "C:\Program Files\Perforce\p4.exe")" $@
+			}
+		fi
 	fi
 fi
