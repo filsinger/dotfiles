14150b8a7c93675a5975e16cecb47679f341abaa
Jason Filsinger <jason.filsinger@gmail.com>
2014-05-01 02:12:59 -0700

    fixing paths in android-sdk.zsh for osx

--- android/android-sdk.zsh
+++ android/android-sdk.zsh
@@ -1,4 +1,17 @@
-if [ -d "/usr/local/opt/android-sdk" ]
-then
-	export ANDROID_HOME=/usr/local/opt/android-sdk
+if [[ $(uname) == 'Darwin' ]] then
+
+	android_sdk_path="$(/usr/local/bin/brew --prefix android-sdk)"
+	android_ndk_path="$(/usr/local/bin/brew --prefix android-ndk)"
+
+	if [ -d "$android_sdk_path" ]
+	then
+		export ANDROID_HOME="$android_sdk_path"
+		export ANDROID_SDK="$android_sdk_path"
+	fi
+
+	if [ -d "$android_ndk_path" ]
+	then
+		export ANDROID_NDK="$android_ndk_path"
+	fi
+
 fi
