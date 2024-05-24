68435552791d10e9bafafbdfb0bef87721a140e9
Jason Filsinger <jason@filsinger.me>
2024-05-24 16:37:24 -0700

    Adding some safety checks when linking environment.d

--- script/link-encironment.sh
+++ script/link-encironment.sh
@@ -4,5 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 target_path=${XDG_CONFIG_HOME:-$HOME/.config}/environment.d
 source_path=$(realpath "$SCRIPT_DIR/../environment.d")
-ln -s "$source_path" "$target_path"
 
+if [ -d $target_path ]; then
+    echo "$target_path already exists, skipping"
+elif [ -e $source_path ]; then
+    echo "$source_path doesn't exist, skipping"
+else
+    ln -s "$source_path" "$target_path"
+fi
