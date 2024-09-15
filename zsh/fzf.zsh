a88d16097d7059ad33230b9ae04fd6f5ffe87516
Jason Filsinger <jason@filsinger.me>
2024-09-14 20:48:51 -0700

    Source fzf in zsh

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,3 +2,6 @@
 
 [[ -f /etc/profile.d/fzf.zsh ]] && source /etc/profile.d/fzf.zsh
 [[ -f /etc/profile.d/fzf-extras.zsh ]] && source /etc/profile.d/fzf-extras.zsh
+
+# Set up fzf key bindings and fuzzy completion
+[[ -x /usr/bin/fzf ]] && source <(fzf --zsh)
