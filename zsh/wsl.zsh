f11f441e5da4cbc334efe09cd462555a1e682ffc
Jason Filsinger <jason@filsinger.me>
2025-10-25 21:31:29 -0700

    zsh: Adding wsl.zsh for wsl specific zsh settings

---
+++ zsh/wsl.zsh
@@ -0,0 +1,20 @@
+# -*- mode: shell-script; -*-
+
+# only do the following if running interactively
+if [[ -n "$PS1" ]]; then
+    if [[ -v WSL_DISTRO_NAME ]]; then
+		# bind HOME and END to beginning and end of line
+		bindkey '\e[1~' beginning-of-line
+		bindkey '\e[4~' end-of-line
+
+		# ignore dll files when autocompleting
+		zstyle ':completion:*:complete:-command-:*' ignored-patterns '*.dll'
+
+		if [[ -e "$(wslpath "C:\Program Files\Perforce\p4.exe")" ]]; then
+			function p4() {
+				export PWD=`wslpath -wa .`
+				"$(wslpath "C:\Program Files\Perforce\p4.exe")" $@
+			}
+		fi
+	fi
+fi
