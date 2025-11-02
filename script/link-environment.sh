b17fde1d6cffb8657cd9fa414a86993dcdfd3eab
Jason Filsinger <jason@filsinger.me>
2025-11-02 03:19:31 -0800

    Improve link-environment.sh linking
    Link the individual `.conf` files rather than the directory.

--- script/link-environment.sh
+++ script/link-environment.sh
@@ -2,13 +2,16 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-target_path=${XDG_CONFIG_HOME:-$HOME/.config}/environment.d
-source_path=$(realpath "$SCRIPT_DIR/../environment.d")
+TARGET_DIRECTORY="${XDG_CONFIG_HOME:-$HOME/.config}/environment.d"
+SOURCE_DIRECTORY=$(realpath "$SCRIPT_DIR/../environment.d")
 
-if [ -d $target_path ]; then
-    echo "$target_path already exists, skipping"
-elif [ ! -e $source_path ]; then
-    echo "$source_path doesn't exist, skipping"
-else
-    ln -s "$source_path" "$target_path"
-fi
+[[ ! -d "$TARGET_DIRECTORY" ]] && mkdir -p "$TARGET_DIRECTORY"
+
+for f in "$SOURCE_DIRECTORY"/*.conf; do
+    TARGET_PATH="$TARGET_DIRECTORY/$(basename "$f")"
+    if [[ -e $TARGET_PATH ]]; then
+        echo "$TARGET_PATH already exists, skipping"
+    else
+        ln -s "$f" "$TARGET_PATH"
+    fi
+done
